8892bf6be50e0e641a2a19597b51f0ee89e74e34
Jacob Grafenstein <grafficmedia@Jacobs-MacBook-Pro.local>
2018-11-03 14:42:58 -0400

    A basic navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import AppNavBar from './components/AppNavbar';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+library.add(faCoffee);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Hello</h1>
+          <AppNavBar />
         </header>
       </div>
     );
---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+class AppNavbar extends Component {
+  state = {
+    isOpen: false
+  }
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <FontAwesomeIcon icon="faCoffee"/>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">SmallBooks</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="https://help.github.com/articles/basic-writing-and-formatting-syntax/">
+                    Browse Stories
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="https://help.github.com/articles/basic-writing-and-formatting-syntax/">
+                    Saved Stories
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="https://help.github.com/articles/basic-writing-and-formatting-syntax/">
+                    Account Settings
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );    
+  }
+}
+
+
+
+export default AppNavbar;
