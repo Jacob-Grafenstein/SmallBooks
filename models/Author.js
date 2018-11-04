21f8ce5b52dae18c7a8aa6895b25a0b6bddbceff
Jake Massmann <jakemassmann@gmail.com>
2018-11-03 23:46:50 -0400

    created user and author model. created user api

---
+++ models/Author.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// create Schema
+const AuthorSchema = new Schema({
+  authorName: {
+    type: String,
+    required: true
+  },
+  authorBiography: {
+    type: String
+  },
+  authorImage: {
+    //Should we require an email?
+    type: String
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now
+  },
+  createdBy: {
+    type: String,
+    required: true
+  },
+  updatedDate: {
+    type: Date,
+    default: Date.now
+  },
+  updatedBy: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = Author = mongoose.model('author', AuthorSchema);
